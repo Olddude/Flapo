be577a2a897cd38ad525187f405d533ddd0db67b
Konstantin Novikov <nov.konstantin@gmail.com>
2021-08-25 11:29:06 +0200

    Update and rename index.ts to app.reducer.ts

--- frontend/src/app/app.reducer.ts
+++ frontend/src/app/app.reducer.ts
@@ -4,16 +4,13 @@ import {
 } from '@ngrx/store';
 import { environment } from '../environments/environment';
 import { routerReducer, RouterState } from '@ngrx/router-store';
-import { postFeatureKey, reducer as postReducer, State as PostState } from './post/post.reducer';
 
 export interface RootState {
   readonly router?: RouterState;
-  readonly [postFeatureKey]?: PostState;
 }
 
 export const reducers: ActionReducerMap<RootState> = {
-  router: routerReducer,
-  [postFeatureKey]: postReducer,
+  router: routerReducer
 };
 
 export const metaReducers: MetaReducer<RootState>[] = !environment.production ? [] : [];
